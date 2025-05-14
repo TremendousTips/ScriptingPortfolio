d9dc592851d7b8df741528bc46f3959af5db291d
TremendousTips <134842823+TremendousTips@users.noreply.github.com>
2025-05-14 15:20:09 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ const projects = [
     title: "Pet Follow System",
     description: "Pets that follow players and avoid collisions using PathfindingService in a simmilar fashion to pet simulator.",
     image: "assets/images/PFS.png",
-    link: "https://github.com/yourusername/roblox-pet-system"
+    link: "https://github.com/yourusername/roblox-pet-system",
+    linkName: "View On Roblox"
   },
   {
     title: "Skill Tree UI",
     description: "Dynamic GUI that handles skill dependencies and animations using TweenService.",
     image: "assets/images/UT.png",
-    link: "https://github.com/yourusername/roblox-skill-tree"
+    link: "https://github.com/yourusername/roblox-skill-tree",
+    linkName: "View On Roblox"
   },
     {
     title: "Ladder to ADMIN! 👑",
     description: "Designed GUI, and scripted all functionality with in the game.",
     image: "assets/images/LTA.png",
-    link: ""
+    link: "https://www.roblox.com/games/75696940007617/Ladder-to-ADMIN",
+    linkName: "View On Roblox"
   },
   
 ];
@@ -30,7 +33,7 @@ projects.forEach(project => {
     <div class="card-content">
       <h2>${project.title}</h2>
       <p>${project.description}</p>
-      <a href="${project.link}" target="_blank" style="color: #00ffd5;">View Project</a>
+      <a href="${project.link}" target="_blank" style="color: #00ffd5;">${project.linkName}</a>
     </div>
   `;
   container.appendChild(card);
